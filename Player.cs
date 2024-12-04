bbd55e46f9f8476a3c5055cb4a77c3ecb2fa672d
agajjar2211 <156842898+agajjar2211@users.noreply.github.com>
2024-12-04 13:46:05 -0500

    1st commit

---
+++ Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace FinalProjectGame
+{
+	internal class Player
+	{
+		public Vector2 Position; // Player position
+		public Vector2 Velocity; // Movement velocity
+		public bool IsJumping; // Tracks jumping state
+
+		public Player(Vector2 startPosition)
+		{
+			Position = startPosition;
+			Velocity = Vector2.Zero;
+			IsJumping = false;
+		}
+	}
+}
