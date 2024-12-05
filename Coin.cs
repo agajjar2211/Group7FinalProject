d7eeb2a1831d7279974d18665650716203e4b88b
Ayushp666 <apatel2320@conestogac.on.ca>
2024-12-04 22:40:22 -0500

    Commit from AyushPatel

---
+++ Coin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace FinalProjectGame
+{
+	internal class Coin
+	{
+		public Vector2 Position; // Coin position
+		public bool Collected; // Whether the coin has been collected
+
+		public Coin(Vector2 position)
+		{
+			Position = position;
+			Collected = false;
+		}
+	}
+}
