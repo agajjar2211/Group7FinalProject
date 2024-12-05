32c3a60edff1787ef9d1cbdec4c01c1d2496fbd4
Tnguyen7917 <Tnguyen7917@conestogac.on.ca>
2024-12-05 12:16:19 -0500

    Create Platform.cs

---
+++ Platform.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace FinalProjectGame
+{
+	internal class Platform
+	{
+		public Rectangle Rectangle; // Dimensions of the platform
+
+		public Platform(Rectangle rectangle)
+		{
+			Rectangle = rectangle;
+		}
+
+	}
+}
